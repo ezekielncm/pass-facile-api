addff389a860ce7fce4d42231c32d2d4f842ee8b
ezekielncm <mnacoulmaezekiel@gmail.com>
2026-03-15 15:30:48 +0000

    //

--- src/Application/Users/Commands/UpdateProfile/UpdateProfileCommandHandle.cs
+++ src/Application/Users/Commands/UpdateProfile/UpdateProfileCommandHandle.cs
@@ -6,23 +6,23 @@ using MediatR;
 using Microsoft.Extensions.Logging;
 
 namespace Application.Users.Commands.UpdateProfile;
-public sealed class UpdateProfileCommandHandle
-    : IRequestHandler<UpdateProfileCommand, Result<UserDto>>
-{
-    private readonly IUserRepository _userRepository;
-    private readonly ILogger _logger;
-    public UpdateProfileCommandHandle(IUserRepository userRepository, ILogger<UpdateProfileCommandHandle> logger)
-    {
-        _userRepository = userRepository;
-        _logger = logger;
-    }
-    public async Task<Result<UserDto>> Handle(UpdateProfileCommand cmd, CancellationToken cancellationToken)
-    {
+//public sealed class UpdateProfileCommandHandle
+//    : IRequestHandler<UpdateProfileCommand, Result<UserDto>>
+//{
+//    private readonly IUserRepository _userRepository;
+//    private readonly ILogger _logger;
+//    public UpdateProfileCommandHandle(IUserRepository userRepository, ILogger<UpdateProfileCommandHandle> logger)
+//    {
+//        _userRepository = userRepository;
+//        _logger = logger;
+//    }
+//    public async Task<Result<UserDto>> Handle(UpdateProfileCommand cmd, CancellationToken cancellationToken)
+//    {
 
-        //var result=_auth.UpdateProfile(cmd);
-        //UserDto user = new UserDto();
-        var rp = await _userRepository.UpdateProfile();                                                 
-        var result = UserDto.FromDomain(rp.User!);
-        return result;
-    }
-}
+//        //var result=_auth.UpdateProfile(cmd);
+//        //UserDto user = new UserDto();
+//        var rp = await _userRepository.UpdateProfile();                                                 
+//        var result = UserDto.FromDomain(rp.User!);
+//        return result;
+//    }
+//}
