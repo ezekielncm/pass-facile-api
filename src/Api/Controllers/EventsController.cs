0c74d99f90ef1f663c7357fc79af61548675647b
ezekielncm <mnacoulmaezekiel@gmail.com>
2026-03-18 09:27:35 +0000

    stagging de certain bug

--- src/Api/Controllers/EventsController.cs
+++ src/Api/Controllers/EventsController.cs
@@ -21,31 +21,31 @@ namespace Api.Controllers
             _logger = logger;
             _mediator = mediator;
         }
-        [HttpPost]
-        [Authorize]
-        public async Task<IActionResult> PostEvents(
-            [FromBody] PostEventRequest rq)
-        {
-            var cmd = new PostEventCommand(
-                rq.Name,
-                rq.Description,
-                rq.Venue,
-                rq.Country,
-                rq.City,
-                rq.AddressLine1,
-                rq.AddressLine2,
-                DateTime.Parse(rq.StartDate),
-                DateTime.Parse(rq.EndDate),
-                rq.CoverImageUrl,
-                int.Parse(rq.Capacity));
-            var result= await _mediator.Send(cmd,CancellationToken.None);
-            return result.Match<IActionResult>(
-                onSuccess:Response=>Ok(Response),
-                onFailure: error => error.Code switch
-                {
-                    var c when c.Contains("Notfound") => NotFound(error),
-                    _ => BadRequest(new { error.Code, error.Message })
-                });
-        }
+        //[HttpPost]
+        //[Authorize]
+        //public async Task<IActionResult> PostEvents(
+        //    [FromBody] PostEventRequest rq)
+        //{
+        //    var cmd = new PostEventCommand(
+        //        rq.Name,
+        //        rq.Description,
+        //        rq.Venue,
+        //        rq.Country,
+        //        rq.City,
+        //        rq.AddressLine1,
+        //        rq.AddressLine2,
+        //        DateTime.Parse(rq.StartDate),
+        //        DateTime.Parse(rq.EndDate),
+        //        rq.CoverImageUrl,
+        //        int.Parse(rq.Capacity));
+        //    var result= await _mediator.Send(cmd,CancellationToken.None);
+        //    return result.Match<IActionResult>(
+        //        onSuccess:Response=>Ok(Response),
+        //        onFailure: error => error.Code switch
+        //        {
+        //            var c when c.Contains("Notfound") => NotFound(error),
+        //            _ => BadRequest(new { error.Code, error.Message })
+        //        });
+        //}
     }
 }
